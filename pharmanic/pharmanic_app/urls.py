5544238654d7272cb0c1e2b76e6b65eb60a4a3f9
Shubham00711 <shubhamsrawool@gmail.com>
2021-10-03 15:05:30 +0530

    added products and login/logout

--- pharmanic/pharmanic_app/urls.py
+++ pharmanic/pharmanic_app/urls.py
@@ -2,18 +2,28 @@ from django.urls import path
 from pharmanic_app import views
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib.auth import views as auth_views
+from .forms import LoginForm
 
 urlpatterns = [
-    path('', views.home),
-    path('product-detail/', views.product_detail, name='product-detail'),
+    path('', views.ProductView.as_view(), name='home'),
+    path('product-detail/<int:pk>', views.ProductDetailView.as_view(), name='product-detail'),
+
     path('cart/', views.add_to_cart, name='add-to-cart'),
     path('buy/', views.buy_now, name='buy-now'),
     path('profile/', views.profile, name='profile'),
     path('address/', views.address, name='address'),
     path('orders/', views.orders, name='orders'),
     path('changepassword/', views.change_password, name='changepassword'),
-    path('mobile/', views.mobile, name='mobile'),
-    path('login/', views.login, name='login'),
-    path('registration/', views.customerregistration, name='customerregistration'),
+    path('tablets_capsules/', views.tablets_capsules, name='tablets_capsules'),
+    path('supplements/', views.supplements, name='supplements'),
+    path('herbs/', views.herbs, name='herbs'),
+    path('health_drinks/', views.health_drinks, name='health_drinks'),
+    path('covid_essentials/', views.covid_essentials, name='covid_essentials'),
+    path('personal_care/', views.personal_care, name='personal_care'),
+
+    path('accounts/login/', auth_views.LoginView.as_view(template_name='app/login.html', authentication_form=LoginForm), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(next_page='login'), name='logout'),
+    path('registration/', views.CustomerRegistrationView.as_view(), name='customerregistration'),
     path('checkout/', views.checkout, name='checkout'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- pharmanic/pharmanic_app/views.py
+++ pharmanic/pharmanic_app/views.py
@@ -1,10 +1,34 @@
 from django.shortcuts import render
+from django.views import View
+from .models import Customer, Product, Cart, OrderPlaced
+from .forms import CustomerRegistrationForm
+from django.contrib import messages
 
-def home(request):
- return render(request, 'app/home.html')
+#def home(request):
+# return render(request, 'app/home.html')
 
-def product_detail(request):
- return render(request, 'app/productdetail.html')
+class ProductView(View):
+	def get(self, request):
+		tablets_capsules = Product.objects.filter(category='TC')
+		supplements = Product.objects.filter(category='S')
+		general = tablets_capsules | supplements
+		herbs = Product.objects.filter(category='H')
+		health_drinks = Product.objects.filter(category='HD')
+		ayurvedic = herbs | health_drinks
+		covid_essentials = Product.objects.filter(category='CE')
+		personal_care = Product.objects.filter(category='PC')
+		healthcare = covid_essentials | personal_care
+		return render(request, 'app/home.html', {'tablets_capsules' : tablets_capsules,'supplements' : supplements,'general' : general,'herbs' : herbs,'health_drinks' : health_drinks,'ayurvedic' : ayurvedic,'covid_essentials' : covid_essentials,'personal_care' : personal_care,'healthcare' : healthcare })
+
+
+#def product_detail(request):
+# return render(request, 'app/productdetail.html')
+
+class ProductDetailView(View):
+	def get(self, request, pk):
+		product = Product.objects.get(pk=pk)
+		return render(request, 'app/productdetail.html', {'product' : product})
+
 
 def add_to_cart(request):
  return render(request, 'app/addtocart.html')
@@ -24,14 +48,41 @@ def orders(request):
 def change_password(request):
  return render(request, 'app/changepassword.html')
 
-def mobile(request):
- return render(request, 'app/mobile.html')
+def tablets_capsules(request):
+	tablets_capsules = Product.objects.filter(category='TC')
+	return render(request, 'app/tablets_capsules.html', {'tablets_capsules' : tablets_capsules})
 
-def login(request):
- return render(request, 'app/login.html')
+def supplements(request):
+	supplements = Product.objects.filter(category='S')
+	return render(request, 'app/supplements.html', {'supplements' : supplements})
 
-def customerregistration(request):
- return render(request, 'app/customerregistration.html')
+def herbs(request):
+	herbs = Product.objects.filter(category='H')
+	return render(request, 'app/herbs.html', {'herbs' : herbs})
+
+def health_drinks(request):
+	health_drinks = Product.objects.filter(category='HD')
+	return render(request, 'app/health_drinks.html', {'health_drinks' : health_drinks})
+
+def covid_essentials(request):
+	covid_essentials = Product.objects.filter(category='CE')
+	return render(request, 'app/covid_essentials.html', {'covid_essentials' : covid_essentials})
+
+def personal_care(request):
+	personal_care = Product.objects.filter(category='PC')
+	return render(request, 'app/personal_care.html', {'personal_care' : personal_care})
+
+class CustomerRegistrationView(View):
+	def get(self, request):
+		form = CustomerRegistrationForm()
+		return render(request, 'app/customerregistration.html', {'form' : form})
+	
+	def post(self, request):
+		form = CustomerRegistrationForm(request.POST)
+		if form.is_valid():
+			messages.success(request, 'Congratulations!! Registered Successfully')
+			form.save()
+		return render(request, 'app/customerregistration.html', {'form' : form})
 
 def checkout(request):
  return render(request, 'app/checkout.html')
